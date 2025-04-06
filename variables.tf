a45bf7db42bdc1938aa67dfbeb5b91c5bea979ac
Kseniya <politiko-ks@narod.ru>
2025-04-06 14:03:22 +0300

    add virtual machines

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "flow" {
+    type = string
+    default = "24-01"
+}
+
+variable "cloud_id" {
+    #name = "organization-politiko-ks"
+    type = string
+    default = "b1gsr925rdnh4d4obuqu"
+}
+variable "folder_id" {
+    #name = "diplom-pks"
+    type = string
+    default = "b1g1nvf8fhbl914bbl81"
+}
+
+variable "bastion" {
+    type = map(number)
+    default = {
+        cores = 2
+        memory = 1
+        core_fraction = 20
+    }
+}
